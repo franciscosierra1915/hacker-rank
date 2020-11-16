29f2d7daef21a00c502893f2502a90665cebfba0
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-11-16 13:41:22 -0600

    swaps

--- warm-up-challenges/minimum-swaps.js
+++ warm-up-challenges/minimum-swaps.js
@@ -1,16 +1,36 @@
 
-const arr = [2, 31, 1, 38, 29, 5, 44, 6, 12, 18, 39, 9, 48, 49, 13, 11, 7, 27, 14, 33, 50, 21, 46, 23, 15, 26, 8, 47, 40, 3, 32, 22, 34, 42, 16, 41, 24, 10, 4, 28, 36, 30, 37, 35, 20, 17, 45, 43, 25, 19];
+const arr = [1, 3, 5, 2, 4, 6, 7]
 // const arr = [1, 2, 3, 4, 5, 6, 7];
 
 function minimumSwaps(arr) {
+
     let swaps = 0;
-    let myarr = [...arr].sort((a, b) => a - b);
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] !== myarr[i]){
-            swaps++
+    for (let i = 0; i < arr.length; i++){
+        let currentElement = arr[i];
+        let position =  i + 1;
+        if( currentElement !== position){
+            let targetIndex
+            for(let j = 0; j < arr.length; j++){
+                if(arr[j] === position){
+                    targetIndex = j;
+                    break
+                }
+            }
+        arr[targetIndex] = currentElement;
+        arr[i] = position;
+        swaps += 1
         }
+
     }
-    return (swaps - 1)
+    return swaps
+
+    // let myarr = [...arr].sort((a, b) => a - b);
+    // for(let i = 0; i < arr.length; i++){
+    //     if(arr[i] !== myarr[i]){
+    //         swaps++
+    //     }
+    // }
+    // return (swaps - 1)
     // for (let i = 0; i < arr.length; i++) {
     //     let currentNum = arr[i];
     //     let correctValue = i + 1;
@@ -33,4 +53,4 @@ function minimumSwaps(arr) {
     // return swaps
 }
 
-console.log(minimumSwaps(arr))
+minimumSwaps(arr)
