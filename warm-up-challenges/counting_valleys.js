109b6818a85b752631319227e68adbe6880589fc
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-11-19 19:41:00 -0600

    sorted arayys merged

--- warm-up-challenges/counting_valleys.js
+++ warm-up-challenges/counting_valleys.js
@@ -27,3 +27,19 @@ function countingValleys(steps, path) {
 }
 
 countingValleys(8, "UDDDUDUU")
+
+    // let sorted = meetings.sort((a, b) => a[0] - b[0]);
+    // let mergedArray = []
+
+    // for(let i = 0; i < sorted.length; i++){
+    //     let j = 1
+    //     if(sorted[i][1] > sorted[j][0]){
+    //         mergedArray = [...mergedArray, [sorted[i][0], sorted[j][1]]]
+    //         i++;
+    //         j += 2
+    //     } else {
+    //         mergedArray = [...mergedArray, sorted[i]]
+    //     }
+    // }
+    // console.log(mergedArray)
+    // console.log(sorted)
---
+++ warm-up-challenges/overlaping-meetings.js
@@ -0,0 +1,72 @@
+
+const meetings = [ [9,14], [17,20], [15,18], [6,9] ]
+// const meetings = [[6,9], [9,14], [15,16], [17,20]]
+
+// Output [[6,14], [15,20]]
+
+function mergeMeetings(meetings){
+
+    // let sorted = meetings.sort((a, b) => a[0] - b[0]);
+
+
+
+}
+
+mergeMeetings(meetings)
+
+const arrays = [[1,2,3,4,5], [-1,0]]
+
+function mergeSorted(arrays){
+    let mergedList = [];
+    // let count = 0;
+    // for (let i = 0; i < arrays.length; i++) {
+    //  // for (let j = 0; j < arrays[i].length; j++) {
+    //     //     for (let k = 0; k < arrays[i + 1]; k++) {
+
+    //     //     }
+    //     // }
+    //     while (count < arrays[i].length || count < arrays[i + 1].length) {
+    //         let pointer1 = arrays[i][i];
+    //         let pointer2 = arrays[i + 1][i];
+    //     }
+    // }
+    for(let i = 0; i < arrays.length; i++){
+        mergedList = [...mergedList, ...arrays[i]];
+    }
+    mergedList.sort((a,b) => a - b)
+    console.log(mergedList)
+}
+
+mergeSorted(arrays)
+
+
+// function mergeSorted(arrays){
+//   const elementIdxs = arrays.map(() => 0);
+//   console.log(elementIdxs);
+// }
+
+// mergeSorted(arrays)
+const arrays = [[1, 5, 9, 21], [-1, 0], [-124, 81, 121], [3, 6, 12, 20, 150]]
+
+const sortedList = [-124];
+
+const elementIdxs = [0, 0, 1, 0];
+
+const smallestItems = [{arrayIdx: 0, num: 1}, {arrayIdx: 1, num: -1}, {arrayIdx: 2, num: -124}, {arrayidx: 3, num: 3}];
+
+const nextItem = {arrayIdx: 2, num: -124}
+
+const meetings = [ [9,14], [17,20], [15,18], [6,9] ]
+// const meetings = [[6,9], [9,14], [15,16], [17,20]]
+
+// Output [[6,14], [15,20]]
+
+function mergeMeetings(meetings){
+
+    // let sorted = meetings.sort((a, b) => a[0] - b[0]);
+
+
+
+}
+
+mergeMeetings(meetings)
