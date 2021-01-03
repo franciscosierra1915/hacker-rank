59dc76627953c7283030b86cc4d2d1f95d0c2632
franciscosierra1915 <fransierra.s.15@gmail.com>
2021-01-03 16:41:55 -0600

    counting objects

---
+++ warm-up-challenges/counting-objects.js
@@ -0,0 +1,13 @@
+const objects = [{x: 1, y: 1}, {x: 2, y: 3}, {x: 3, y: 3}, {x: 3, y: 4}, {x: 4, y: 5}]
+
+function getCount(objects) {
+    let count = 0;
+    for(let i = 0; i < objects.length; i++){
+        if(objects[i].x === objects[i].y){
+            count++
+        }
+    }
+    return count;
+}
+
+console.log(getCount(objects));
